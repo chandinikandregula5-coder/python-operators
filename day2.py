5ff6af24fb969ffaf359440a1aece966a3c959b2
chandini <chandinikandregula5@gmail.com>
2026-05-14 20:50:04 +0530

    Add files via upload
    day 2 practice of python operators

---
+++ day2.py
@@ -0,0 +1,90 @@
+'''
+1.operators:
+
+ 1.ARITHMETIC OPERATOR
+ +,-,*,/,//,%,**
+ -------------------------
+ 
+ 2.ASSIGNMENT OPERATOR
+ 
+ 3.COMPARISION OPERATOR
+ ==(*imp looks for whether both values are equal or not)!
+ 
+ 4.LOGICAL OPERATOR
+ ----this is used to check both should be true 
+ 5.MEMBERSHIP OPERATOR
+ 6.IDENTITY OPERATOR
+ is, is not
+ is--> this operator looks for the object same or not
+ --------------------------
+ 
+ 7.BITWISE OPERATOR
+ 
+'''
+a=[1,2]
+b=[1,2]
+print(a==b)
+print(id(a))==
+print(id(b))
+a= 5
+if a % 3 == 0 or a % 5 ==0:
+    print ("true")
+a= 7
+b=[1,2,7,8]
+
+print(a in b)
+a=7
+b=[1,2,7,8]
+=print(a not in b)
+'''
+'''
+print(2*3)
+print(5%4)
+print(10**4)
+print(105//6)
+print(105/6)
+'''
+a = 9 # imutable
+b = 7
+print(a+b)
+
+'''
+'''
+any = " @&,."
+print(any)
+any = "python is a language"
+print(any.replace("python","java"))
+print(any)
+SPLIT------used to separate into parts,and split based on the substring where before substring is one index and after is another index in the list
+'''
+any=("python is a language")
+print(any.split("$"))
+'''
+'''
+any = " python is a language"
+print(len(any))
+'''
+slicing() gives me access to get particular index from the string.
+-----------
+'''
+any = "python is a language"
+print(any[3:11])
+'''
+'''
+syntax--variable _name[index position]......
+any = "python is a language"
+print(any[7])
+print(any.index("ang"))
+
+
+
+
+
+
+
+
+
+
+
+
+
